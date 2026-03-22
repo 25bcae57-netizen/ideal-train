d81dc2582103d1c21c0dd946a472adcd3ecccd4d
25bcae57-netizen <25bcae57-netizen@users.noreply.github.com>
2026-03-22 09:53:06 +0000

    deploy: a54b7de27571e4b2d4020afa999120a035b99883

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,8 @@ app.get("/delete/:id", (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
